7f9bf0a4b353d5be6557d063f1426979a53bbfd2
Hemraj-7 <mhemraj814@gmail.com>
2024-06-07 15:36:06 +0530

    Calculator

--- Task 3 - Calculator/script.js
+++ Task 3 - Calculator/script.js
@@ -1,4 +1,22 @@
-let screen = document.querySelector(".display-screen");
-let btns = document.querySelector(".buttons");
-let output = "";
-console.log(screen, btns)
+const display = document.querySelector(".display-screen");
+
+function displayItem(input){
+    display.value += input;
+}
+
+function displayClear() {
+    display.value = "";
+}
+
+function deleteItem(){
+    display.value = display.value.toString().slice(0, -1);
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "";
+    }
+}
